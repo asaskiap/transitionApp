fd79908d46e0136c970a54810cd5c09902bd9595
asaskiap <alannapfeiffer@gmail.com>
2021-11-23 12:26:00 +0100

    added answer change for entscheidungsbaum

--- screens/entscheidungsbaumGoScreen.js
+++ screens/entscheidungsbaumGoScreen.js
@@ -5,7 +5,7 @@ import Colors from './../assets/colors';
 import CardEB from '../components/entscheidungsBaumComponents/cardEB';
 import ScrollableScreenContainer from '../components/scrollableScreen';
 import {HeaderEB} from '../components/entscheidungsBaumComponents/headerEB';
-import ArticleImage from '../components/articleComponents/articleImage';
+import ArticleIllustration from '../components/articleComponents/articleIllustration';
 import {
   CouncellingInvite1,
   CouncellingInvite2,
@@ -16,152 +16,164 @@ import colors from './../assets/colors';
 
 class EntscheidungsbaumGoScreen extends React.Component {
   state = {
-    q1: false,
-    q2: false,
-    q3: false,
+    displayImage: true,
+    display_q1: true,
+    q1_yes: false,
+    q1_no: false,
+    display_q2: false,
+    q2_yes: false,
+    q2_no: false,
+    display_q3: false,
+    q3_yes: false,
+    q3_no: false,
     displayCouncellingInvite1: false,
     displayCouncellingInvite2: false,
-    displayCouncellingInvite3: false
+    displayCouncellingInvite3: false,
+    displayFinalCouncellingInvite: false
   };
 
   set_q1 = (ans) => {
-    console.log(ans);
     this.setState({
-      q1: ans,
-      displayCouncellingInvite1: !ans
+      q1_yes: ans,
+      display_q2: ans,
+      q1_no: !ans,
+      displayImage: false,
+      displayCouncellingInvite1: !ans,
+      //reset
+      q2_yes: false,
+      q2_no: false,
+      q3_yes: false,
+      q3_no: false,
+      displayCouncellingInvite2: false,
+      displayCouncellingInvite3: false,
+      displayFinalCouncellingInvite: false
     });
-    console.log(this.state.q1, this.state.displayCouncellingInvite1);
   };
 
   set_q2 = (ans) => {
     this.setState({
-      q2: ans,
-      displayCouncellingInvite2: !ans
+      q2_yes: ans,
+      display_q3: ans,
+      q2_no: !ans,
+      displayCouncellingInvite2: !ans,
+      // reset
+      q3_yes: false,
+      q3_no: false,
+      displayCouncellingInvite1: false,
+      displayCouncellingInvite3: false,
+      displayFinalCouncellingInvite: false
     });
   };
 
   set_q3 = (ans) => {
     this.setState({
-      q3: ans,
-      displayCouncellingInvite3: !ans
+      q3_yes: ans,
+      q3_no: !ans,
+      displayCouncellingInvite3: !ans,
+      displayFinalCouncellingInvite: ans,
+      //reset
+      displayCouncellingInvite2: false,
+      displayCouncellingInvite1: false
     });
   };
 
   render() {
+    console.log(!(this.state.q1_yes || this.state.q1_no));
     return (
       <ScrollableScreenContainer>
         <HeaderEB>Entscheidungsbaum</HeaderEB>
 
         {/* image should only show up if no answer has been given yet: no next question or councelling invite is displayed */}
-        {!(this.state.q1 || this.state.displayCouncellingInvite1) && (
+        {/* {!(this.state.q1 || this.state.displayCouncellingInvite1) && ( */}
+        {this.state.display_q1 && (
           <>
             <CardEB
-              backgroundYes={Colors.secondaryLight}
-              backgroundNo={Colors.accentPale}
+              backgroundYes={
+                !(this.state.q1_yes || this.state.q1_no)
+                  ? Colors.secondaryLight
+                  : this.state.q1_yes
+                  ? Colors.secondary
+                  : Colors.btnDeactivated
+              }
+              backgroundNo={
+                !(this.state.q1_yes || this.state.q1_no)
+                  ? Colors.accentPale
+                  : this.state.q1_no
+                  ? Colors.accentDark
+                  : Colors.btnDeactivated
+              }
               yes={() => this.set_q1(true)}
               no={() => this.set_q1(false)}
             >
               Ich denke über das Ende meiner Karriere nach und weiß schon was
               ich machen möchte
             </CardEB>
-            <ArticleImage
-              image={require('../assets/illustrations/entscheidungsbaumIllustrations/mountain.png')}
-              imageContainerStyle={{width: '90%', alignSelf: 'center'}}
-            />
           </>
         )}
+        {this.state.displayImage && (
+          <ArticleIllustration
+            image={require('../assets/illustrations/entscheidungsbaumIllustrations/mountain.png')}
+          />
+        )}
 
-        {this.state.q1 && (
-          <>
-            <CardEB
-              backgroundYes={Colors.secondary}
-              backgroundNo={Colors.btnDeactivated}
-            >
-              Ich denke über das Ende meiner Karriere nach und weiß schon was
-              ich machen möchte
-            </CardEB>
-            {!(this.state.q2 || this.state.displayCouncellingInvite2) && (
-              <CardEB
-                backgroundYes={Colors.secondaryLight}
-                backgroundNo={Colors.accentPale}
-                yes={() => this.set_q2(true)}
-                no={() => this.set_q2(false)}
-              >
-                Ich weiß auch wo und was ich studieren möchte
-              </CardEB>
-            )}
-          </>
+        {this.state.display_q2 && (
+          <CardEB
+            backgroundYes={
+              !(this.state.q2_yes || this.state.q2_no)
+                ? Colors.secondaryLight
+                : this.state.q2_yes
+                ? Colors.secondary
+                : Colors.btnDeactivated
+            }
+            backgroundNo={
+              !(this.state.q2_yes || this.state.q2_no)
+                ? Colors.accentPale
+                : this.state.q2_no
+                ? Colors.accentDark
+                : Colors.btnDeactivated
+            }
+            yes={() => this.set_q2(true)}
+            no={() => this.set_q2(false)}
+          >
+            Ich weiß auch wo und was ich studieren möchte
+          </CardEB>
         )}
 
         {this.state.displayCouncellingInvite1 && (
-          <>
-            <CardEB
-              backgroundYes={Colors.btnDeactivated}
-              backgroundNo={Colors.accentDark}
-            >
-              Ich denke über das Ende meiner Karriere nach und weiß schon was
-              ich machen möchte
-            </CardEB>
-            <CouncellingInvite1 {...this.props} />
-          </>
+          <CouncellingInvite1 {...this.props} />
         )}
 
-        {this.state.q1 && this.state.q2 && (
-          <>
-            <CardEB
-              backgroundYes={Colors.secondary}
-              backgroundNo={Colors.btnDeactivated}
-            >
-              Ich weiß auch wo und was ich studieren möchte
-            </CardEB>
-            {!(this.state.q3 || this.state.displayCouncellingInvite3) && (
-              <CardEB
-                backgroundYes={Colors.secondaryLight}
-                backgroundNo={Colors.accentPale}
-                yes={() => this.set_q3(true)}
-                no={() => this.set_q3(false)}
-              >
-                Ich habe bereits eine Finanzierungsmöglichkeit
-              </CardEB>
-            )}
-          </>
-        )}
-
-        {this.state.displayCouncellingInvite2 && (
-          <>
-            <CardEB
-              backgroundYes={Colors.btnDeactivated}
-              backgroundNo={Colors.accentDark}
-            >
-              Ich weiß auch wo und was ich studieren möchte
-            </CardEB>
-            <CouncellingInvite2 {...this.props} />
-          </>
-        )}
-
-        {this.state.q3 && (
+        {this.state.display_q3 && (
           <CardEB
-            backgroundYes={Colors.secondary}
-            backgroundNo={Colors.btnDeactivated}
+            backgroundYes={
+              !(this.state.q3_yes || this.state.q3_no)
+                ? Colors.secondaryLight
+                : this.state.q3_yes
+                ? Colors.secondary
+                : Colors.btnDeactivated
+            }
+            backgroundNo={
+              !(this.state.q3_yes || this.state.q3_no)
+                ? Colors.accentPale
+                : this.state.q3_no
+                ? Colors.accentDark
+                : Colors.btnDeactivated
+            }
+            yes={() => this.set_q3(true)}
+            no={() => this.set_q3(false)}
           >
             Ich habe bereits eine Finanzierungsmöglichkeit
           </CardEB>
         )}
 
+        {this.state.displayCouncellingInvite2 && (
+          <CouncellingInvite2 {...this.props} />
+        )}
         {this.state.displayCouncellingInvite3 && (
-          <>
-            <CardEB
-              backgroundYes={Colors.btnDeactivated}
-              backgroundNo={Colors.accentDark}
-            >
-              Ich habe bereits eine Finanzierungsmöglichkeit
-            </CardEB>
-            <CouncellingInvite3 {...this.props} />
-          </>
+          <CouncellingInvite3 {...this.props} />
         )}
-        {this.state.q1 && this.state.q2 && this.state.q3 && (
-          <FinalCouncellingInvite />
-        )}
+
+        {this.state.displayFinalCouncellingInvite && <FinalCouncellingInvite />}
       </ScrollableScreenContainer>
     );
   }
